96842f02ed7ca9c27a552e532fbe3f11f633c34a
Oleg Prikhodko <4.oleg.pr@gmail.com>
2022-07-14 19:53:21 +0600

    add parameter

--- rn-video.py
+++ rn-video.py
@@ -1,9 +1,7 @@
-'''Скрипт переименовывает видеофайлы в текущем каталоге: добавляет к названию файла его продолжительность.'''
-
 import os
 import time
 from mutagen.mp4 import MP4
-
+from sys import argv
 
 def my_time_format(time_string):
     '''Отсекаем нули от строкового формата времени'''
@@ -12,15 +10,20 @@ def my_time_format(time_string):
             return time_string
         time_string = time_string[1:]
 
+list_video_files = []
+if len(argv) > 1:
+    # Если передано имя файла - добавляем его с список
+    list_video_files.append(argv[1])
+else:
+    # Если не передано имя файла - добавляем в список все видеофайлы в данном каталоге
+    list_video_files = [item
+                        for item in os.listdir()
+                        if os.path.isfile(item) and item.endswith(('.mp4', '.avi', '.mkw'))]
 
 answer = input(
     'Будет произведено переименование файлов. Введите "у" или Enter чтобы продолжить: ')
 
 if answer == 'y' or answer == '':
-    list_video_files = [item
-                        for item in os.listdir()
-                        if os.path.isfile(item) and item.endswith(('.mp4', '.avi', '.mkw'))]
-
     for file in list_video_files:
         file_duration_seconds = int(MP4(file).info.length)
         time_string = time.strftime("%H-%M-%S", time.gmtime(file_duration_seconds))
