9ef88e72c2815a305d45d2be08f7081130b82ce4
Oleg Prikhodko <4.oleg.pr@gmail.com>
2023-07-15 22:10:50 +0600

    refactor: add comments and changed the logic

--- rn.py
+++ rn.py
@@ -8,7 +8,7 @@ list_video_files = []
 
 
 def move_files_in_current_dir():
-    '''Перемещам все видеофайлы, которые находятся во вложенных папках в текущий каталог'''
+    '''Move all video files that are in subfolders to the current directory'''
     for adress, dirs, files in os.walk(os.curdir):
         for file in files:
             full = os.path.join(adress, file)
@@ -16,26 +16,19 @@ def move_files_in_current_dir():
                 os.replace(full, file)
 
 
-def my_time_format(time_string):
-    '''Отсекаем нули от строкового формата времени'''
+def seconds_to_string_format(seconds):
+    '''Convert the number of seconds to string format'''
+    time_string = time.strftime("%H-%M-%S", time.gmtime(seconds))
     for i in time_string:
         if (i in '123456789') or (len(time_string) == 4):
-            return time_string
+            break
         time_string = time_string[1:]
 
-
-def add_duration(file_name, time_string):
-    '''Добавляем к названию файла его продолжительность, если она не указана'''
-
-    if not (file_name[-2:].isdigit() and file_name[-3:-2] == '-'):
-        file_name = file_name + ' ' + time_string
-
-    return file_name
+    return time_string
 
 
 def change_bracket(file_name):
-    '''Убираем всю информацию в круглых скобках за исключением качества видеофайла '''
-
+    '''We remove all information in brackets except for the resolution of the video file'''
     brackets_start_position = file_name.rfind('(')
     brackets_end_position = file_name.rfind(')')
     file_quality_position = file_name.find('p', brackets_start_position, brackets_start_position + 7)
@@ -45,39 +38,42 @@ def change_bracket(file_name):
     return file_name
 
 
-if __name__ == "__main__":
+answer = input(
+    'The file(s) will be renamed. Type "y" or Enter to continue: ')
 
-    answer = input(
-        'Будет произведено переименование файла(ов). Введите "у" или Enter чтобы продолжить: ')
+if answer == 'y' or answer == '':
 
-    if answer == 'y' or answer == '':
+    if '-dir' in argv:
+        move_files_in_current_dir()
 
-        if '-dir' in argv:
-            move_files_in_current_dir()
+    if '-f' in argv:
+        current_item_index = argv.index('-f') + 1
+        while current_item_index < len(argv):
+            list_video_files.append(argv[current_item_index])
+            current_item_index += 1
 
-        if '-f' in argv:
-            current_item_index = argv.index('-f') + 1
-            while current_item_index < len(argv):
-                list_video_files.append(argv[current_item_index])
-                current_item_index += 1
+    if len(list_video_files) == 0:
+        list_video_files = [item
+                            for item in os.listdir()
+                            if os.path.isfile(item) and item.endswith(searched_files)]
 
-        if len(list_video_files) == 0:
-            list_video_files = [item
-                                for item in os.listdir()
-                                if os.path.isfile(item) and item.endswith(searched_files)]
+    sum_seconds = 0
+    for file in list_video_files:
+        file_name = file[:-4]
+        file_extension = file[-4:]
 
-        for file in list_video_files:
-            file_duration_seconds = int(MP4(file).info.length)
-            time_string = time.strftime("%H-%M-%S", time.gmtime(file_duration_seconds))
-            time_string = my_time_format(time_string)
-            file_name = file[:-4]
-            file_extension = file[-4:]
+        if '-c' in argv:
+            file_name = file_name.capitalize()
 
-            if '-c' in argv:
-                file_name = file_name.capitalize()
+        if '-b' in argv:
+            file_name = change_bracket(file_name)
 
-            if '-b' in argv:
-                file_name = change_bracket(file_name)
+        file_duration_seconds = int(MP4(file).info.length)
+        sum_seconds += file_duration_seconds
 
-            file_name = add_duration(file_name, time_string)
-            os.rename(file, file_name + file_extension)
+        # Add the duration of the file to the file name if it is not specified
+        if not (file_name[-2:].isdigit() and file_name[-3:-2] == '-'):
+            time_string = seconds_to_string_format(file_duration_seconds)
+            os.rename(file, file_name + ' ' + time_string + file_extension)
+
+    print(f'Total time of video files: {seconds_to_string_format(sum_seconds)}')
